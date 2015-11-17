1a00b445281e1aafb6b19055a0dfed797d7361cc
alumno <alumno@2A5PC15>
2015-11-17 10:45:17 +0100

    Todo usuario comentado, borrar BD por checksum de flyway

--- web/usuario/usuario-Service.js
+++ web/usuario/usuario-Service.js
@@ -1,58 +1,58 @@
-UsuarioService.$inject = ['$http'];
-
-function UsuarioService($http) {
-
-    this.get = function (idUsuario) {
-        var response = $http({
-            method: "GET",
-            url: "../banco-api/api/usuario" + idUsuario
-        });
-        return response;
-    };
-
-    this.insert = function (usuario) {
-        var response = $http({
-            method: "POST",
-            url: "../banco-api/api/usuario",
-            data: usuario
-        }
-        );
-        return response;
-    };
-
-    this.update = function (usuario) {
-        var response = $http({
-            method: "PUT",
-            url: "../banco-api/api/usuario" + usuario.idUsuario,
-            data: usuario
-        });
-        return response;
-    };
-
-    this.delete = function (idUsuario) {
-        var response = $http({
-            method: "DELETE",
-            url: "../banco-api/api/usuario" + idUsuario
-        });
-        return response;
-    };
-
-    this.find = function () {
-        var response = $http({
-            method: "GET",
-            url: "../banco-api/api/usuario"
-        });
-        return response;
-    };
-
-    this.findByNombre = function (nombre) {
-        var response = $http({
-            method: "GET",
-            url: "../banco-api/api/usuario/?nombre=" + nombre
-        });
-        return response;
-    };
-
-}
-
-app.service("entidadBancariaService", EntidadBancariaService);
+//UsuarioService.$inject = ['$http'];
+//
+//function UsuarioService($http) {
+//
+//    this.get = function (idUsuario) {
+//        var response = $http({
+//            method: "GET",
+//            url: "../banco-api/api/usuario" + idUsuario
+//        });
+//        return response;
+//    };
+//
+//    this.insert = function (usuario) {
+//        var response = $http({
+//            method: "POST",
+//            url: "../banco-api/api/usuario",
+//            data: usuario
+//        }
+//        );
+//        return response;
+//    };
+//
+//    this.update = function (usuario) {
+//        var response = $http({
+//            method: "PUT",
+//            url: "../banco-api/api/usuario" + usuario.idUsuario,
+//            data: usuario
+//        });
+//        return response;
+//    };
+//
+//    this.delete = function (idUsuario) {
+//        var response = $http({
+//            method: "DELETE",
+//            url: "../banco-api/api/usuario" + idUsuario
+//        });
+//        return response;
+//    };
+//
+//    this.find = function () {
+//        var response = $http({
+//            method: "GET",
+//            url: "../banco-api/api/usuario"
+//        });
+//        return response;
+//    };
+//
+//    this.findByNombre = function (nombre) {
+//        var response = $http({
+//            method: "GET",
+//            url: "../banco-api/api/usuario/?nombre=" + nombre
+//        });
+//        return response;
+//    };
+//
+//}
+//
+//app.service("entidadBancariaService", EntidadBancariaService);
